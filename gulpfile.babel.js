f87bc55d7e2c01ef3adc988c584900f7d9dc3fbf
burczu <b.dybowski@gmail.com>
2016-02-22 14:28:57 +0100

    const using added

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -2,10 +2,12 @@ import gulp from 'gulp';
 import less from 'gulp-less';
 import path from 'path';
 
+const lessIncludes = [path.join(__dirname, 'layout', 'includes')];
+
 gulp.task('less', () => {
   return gulp.src('./layout/**/*.less')
     .pipe(less({
-      paths: [path.join(__dirname, 'layout', 'includes')]
+      paths: lessIncludes
     }))
     .pipe(gulp.dest('./build/css'));
 });
